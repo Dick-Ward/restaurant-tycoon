d5b6195e80bb4b831794806fd0ff4820cf05f3b2
Chris Metzger <chrismetzgr@gmail.com>
2018-11-21 17:39:11 -0500

    Set up console

---
+++ config/environment.rb
@@ -0,0 +1,4 @@
+require 'pry'
+require_relative '../lib/restaurant.rb'
+require_relative '../lib/location.rb'
+
--- tools/console.rb
+++ tools/console.rb
@@ -1,3 +1,3 @@
-require_relative 'environment.rb'
+require_relative '../config/environment.rb'
 
 Pry.start
